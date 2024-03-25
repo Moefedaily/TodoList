9c845057e0b2dab4e384d8ba228533b6b91cda42
unknown <mohammedfedaily@gmail.com>
2024-03-25 21:56:23 +0100

    Last update

--- App/Views/register.php
+++ App/Views/register.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/047f41f5d9.js" crossorigin="anonymous"></script>
 </head>
 
