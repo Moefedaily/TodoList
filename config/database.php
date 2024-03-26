0dbe172d2597c6a783969dc421328835b9f28457
unknown <mohammedfedaily@gmail.com>
2024-03-26 01:12:13 +0100

    update Db

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
  
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'todolist');
-    define('DB_USER', 'todolist');
-    define('DB_PWD', 'password123');
+    define('DB_NAME', 'my_webapp__31');
+    define('DB_USER', 'my_webapp__31');
+    define('DB_PWD', 'RptBG1OMurih1y4MM3H95R6KcI1Zbj');
     
--- todolist.sql
+++ todolist.sql
@@ -18,8 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todolist`
---
+-- Database: `my_webapp__31`--
 
 -- --------------------------------------------------------
 
